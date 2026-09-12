8eefa4b0e8f99764c857159ac401da97217c7deb
Saida-85 <saidanosratorfa2000@gmail.com>
2026-09-12 17:13:37 +0600

    modified:   package-lock.json         modified:   package.json         modified:   src/App.tsx   src/components/StackSidebar.tsx         src/components/TechnologyCard.tsx         src/components/TechnologySection.tsx         src/data/

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Banner from "./components/Banner";
 import Nav from "./components/Nav";
+import TechnologySection from "./components/TechnologySection";
 
 function App() {
   return (
     <>
       <Nav />
       <Banner />
+      <TechnologySection />
     </>
   );
 }
